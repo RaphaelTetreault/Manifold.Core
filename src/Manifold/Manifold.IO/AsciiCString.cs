d14f5b8577854373d0f1ce1870c391df4c0ae33b
RaphaelTetreault <raphael.tetreault@outlook.com>
2022-03-16 17:03:38 -0400

    Add AsciiCString, PaddingUtility for compatibility with new GameCube.AmusementVision

---
+++ src/Manifold/Manifold.IO/AsciiCString.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Manifold.IO
+{
+    /// <summary>
+    /// Simple wrapper class for string to encode and decode a C-style null-terminated
+    /// string in ASCII format.
+    /// </summary>
+    public class AsciiCString : CString
+    {
+        // CONSTANTS
+        private static readonly Encoding ascii = Encoding.ASCII;
+
+        // PROPERTIES
+        public override Encoding Encoding => ascii;
+
+        // CONSTRUCTORS
+        public AsciiCString() : base() { }
+        public AsciiCString(string value) : base(value) { }
+
+        // OPERATORS
+        public static implicit operator string(AsciiCString cstr) => cstr.Value;
+        public static implicit operator AsciiCString(string str) => new(str);
+    }
+}
---
+++ src/Manifold/Manifold.IO/PaddingUtility.cs
@@ -0,0 +1,36 @@
+﻿namespace Manifold.IO
+{
+    public static class PaddingUtility
+    {
+        public static int Align(int value, int alignment) => (int)AlignSigned(value, alignment);
+        public static long Align(long value, long alignment) => AlignSigned(value, alignment);
+        public static uint Align(uint value, uint alignment) => (uint)AlignUnsigned(value, alignment);
+        public static ulong Align(ulong value, ulong alignment) => AlignUnsigned(value, alignment);
+
+        private static long AlignSigned(long value, long alignment)
+        {
+            if (value < 0)
+                throw new ArgumentException($"Argument `{nameof(value)}` cannot be negative.");
+            if (alignment < 1)
+                throw new ArgumentException($"Argument `{nameof(alignment)}` must be greater than 1.");
+
+            // Get number of bytes needed to aligment
+            var diff = value % alignment;
+            // If delta is 0, return 0; else length to align is (alignment - diff)
+            var length = diff == 0 ? 0 : alignment - diff;
+            return length;
+        }
+
+        private static ulong AlignUnsigned(ulong value, ulong alignment)
+        {
+            if (alignment < 1)
+                throw new ArgumentException($"Argument `{nameof(alignment)}` must be greater than 1.");
+
+            // Get number of bytes needed to aligment
+            var diff = value % alignment;
+            // If delta is 0, return 0; else length to align is (alignment - diff)
+            var length = diff == 0 ? 0 : alignment - diff;
+            return length;
+        }
+    }
+}
