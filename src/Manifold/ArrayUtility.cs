f5f66720469f0f9118782ec571c8d9e178c9fe66
Raphaël Tétreault <raphaeltetreault@hotmail.com>
2022-07-28 22:56:05 -0400

    Add some array utilities

---
+++ src/Manifold/ArrayUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manifold
+{
+    public static class ArrayUtility
+    {
+        public static T[] DefaultArray<T>(T defaultValue, int length)
+        {
+            var array = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = defaultValue;
+            }
+            return array;
+        }
+    }
+}
--- src/Manifold/Extensions.System.Array.cs
+++ src/Manifold/Extensions.System.Array.cs
@@ -33,5 +33,13 @@ namespace Manifold
             string formattedString = i.ToString().PadLeft(width, paddingChar);
             return formattedString;
         }
+
+        public static void Populate<T>(this T[] array, T value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = value;
+            }
+        }
     }
 }
