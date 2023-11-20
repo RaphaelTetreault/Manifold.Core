c60f3108dc81b330d907d3d6bd7fce04d49d4a3c
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2023-11-20 00:14:27 -0500

    Add GenericCString (pass it encoding)

--- src/Manifold.IO/AsciiCString.cs
+++ src/Manifold.IO/AsciiCString.cs
@@ -2,6 +2,8 @@
 
 namespace Manifold.IO
 {
+    // TODO: this should be a windows12## encoding, as mentioned in Dolphin's source.
+
     /// <summary>
     ///     Simple wrapper class for string to encode and decode a C-style null-terminated
     ///     string in ASCII format.
@@ -9,7 +11,7 @@ namespace Manifold.IO
     public class AsciiCString : CString
     {
         // CONSTANTS
-        private static readonly Encoding ascii = Encoding.ASCII;
+        public static readonly Encoding ascii = Encoding.ASCII;
 
         // PROPERTIES
         public override Encoding Encoding => ascii;
---
+++ src/Manifold.IO/GenericCString.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Manifold.IO
+{
+    public class GenericCString : CString
+    {
+        // PROPERTIES
+        public override Encoding Encoding { get; }
+
+        // CONSTRUCTORS
+        public GenericCString(Encoding encoding) : base()
+        {
+            Encoding = encoding;
+        }
+        public GenericCString(Encoding encoding, string value) : base(value)
+        {
+            Encoding = encoding;
+        }
+
+        // OPERATORS
+        public static implicit operator string(GenericCString cstr) => cstr.Value;
+    }
+}
