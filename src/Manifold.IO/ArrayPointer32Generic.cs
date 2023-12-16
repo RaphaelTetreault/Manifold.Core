80f0b889baf22e3110e4bf482b4ca7d99a102581
Raphaël Tétreault <150467892+MohawkRaphaelT@users.noreply.github.com>
2023-12-16 17:19:34 -0500

    Add generic Pointer32 and ArrayPointer32 types which enclose the referenced type

---
+++ src/Manifold.IO/ArrayPointer32Generic.cs
@@ -0,0 +1,102 @@
+﻿namespace Manifold.IO
+{
+    public class ArrayPointer32<TBinarySerializable> :
+        IBinarySerializable,
+        IPointer
+        where TBinarySerializable : IBinarySerializable, new()
+    {
+        // FIELDS
+        private ArrayPointer arrayPointer;
+        private TBinarySerializable[] array;
+
+        // PROPERTIES
+        public int Address => arrayPointer.address;
+        public ArrayPointer ArrayPtr => arrayPointer;
+        public bool IsNotNull => arrayPointer.IsNotNull;
+        public bool IsNull => arrayPointer.IsNull;
+        public int Length => arrayPointer.length;
+        public TBinarySerializable[] Array
+        {
+            get => array;
+            set => array = value;
+        }
+
+        // INDEXERS
+        public TBinarySerializable this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+
+        // OPERATORS
+        public static implicit operator ArrayPointer(ArrayPointer32<TBinarySerializable> arrayPointer32Generic)
+        {
+            return arrayPointer32Generic.arrayPointer;
+        }
+        public static implicit operator TBinarySerializable[](ArrayPointer32<TBinarySerializable> pointer32Generic)
+        {
+            return pointer32Generic.array;
+        }
+        public static implicit operator ArrayPointer32<TBinarySerializable>(ArrayPointer arrayPointer)
+        {
+            var newValue = new ArrayPointer32<TBinarySerializable>(arrayPointer.address, arrayPointer.length);
+            return newValue;
+        }
+        public static implicit operator ArrayPointer32<TBinarySerializable>(TBinarySerializable value)
+        {
+            var newValue = new ArrayPointer32<TBinarySerializable>(value);
+            return newValue;
+        }
+
+        // CONSTRUCTORS
+        public ArrayPointer32()
+        {
+            array = System.Array.Empty<TBinarySerializable>();
+        }
+        public ArrayPointer32(int address, int length)
+        {
+            arrayPointer = new ArrayPointer(length, address);
+            array = new TBinarySerializable[length];
+        }
+        public ArrayPointer32(params TBinarySerializable[] values)
+        {
+            arrayPointer = new ArrayPointer(values.Length, Pointer.Null);
+            array = values;
+        }
+
+        // METHODS
+        public void Deserialize(EndianBinaryReader reader)
+        {
+            reader.Read(ref arrayPointer);
+        }
+
+        public void DeserializeArray(EndianBinaryReader reader)
+        {
+            // Get address to return to after deserializing
+            Pointer currentAddress = reader.GetPositionAsPointer();
+
+            // Jump to address and deserialize values
+            reader.JumpToAddress(arrayPointer);
+            reader.Read(ref array, arrayPointer.length);
+
+            // Return to initial address
+            reader.JumpToAddress(currentAddress);
+        }
+
+        public void Serialize(EndianBinaryWriter writer)
+        {
+            writer.Write(arrayPointer);
+        }
+
+        public void SerializeArray(EndianBinaryWriter writer)
+        {
+            // Ensure correct base address and length is stored
+            arrayPointer.address = writer.GetPositionAsPointer();
+            arrayPointer.length = array.Length;
+
+            // Write all values
+            writer.Write(array);
+        }
+
+    }
+}
---
+++ src/Manifold.IO/Pointer32.cs
@@ -0,0 +1,91 @@
+﻿namespace Manifold.IO
+{
+    public sealed class Pointer32<TBinarySerializable> :
+        IBinarySerializable,
+        IPointer
+        where TBinarySerializable : IBinarySerializable, new()
+    {
+        // FIELDS
+        private Pointer pointer;
+        private TBinarySerializable value;
+
+        // PROPERTIES
+        public int Address => pointer.address;
+        public bool IsNotNull => pointer.IsNotNull;
+        public bool IsNull => pointer.IsNull;
+        public bool IsValueNull => value == null;
+        public bool IsValueNotNull => value != null;
+        public TBinarySerializable Value
+        {
+            get => value;
+            set => this.value = value;
+        }
+
+        // OPERATORS
+        public static implicit operator Pointer(Pointer32<TBinarySerializable> pointer32Generic)
+        {
+            return pointer32Generic.pointer;
+        }
+        public static implicit operator TBinarySerializable(Pointer32<TBinarySerializable> pointer32Generic)
+        {
+            return pointer32Generic.value;
+        }
+        public static implicit operator Pointer32<TBinarySerializable>(Pointer address)
+        {
+            var newValue = new Pointer32<TBinarySerializable>(address);
+            return newValue;
+        }
+        public static implicit operator Pointer32<TBinarySerializable>(TBinarySerializable value)
+        {
+            var newValue = new Pointer32<TBinarySerializable>(value);
+            return newValue;
+        }
+
+        // CONSTRUCTORS
+        public Pointer32()
+        {
+            value = new TBinarySerializable();
+        }
+        public Pointer32(Pointer pointer) : this()
+        {
+            this.pointer = pointer;
+        }
+        public Pointer32(TBinarySerializable value)
+        {
+            this.value = value;
+        }
+
+        // METHODS
+        public void Deserialize(EndianBinaryReader reader)
+        {
+            reader.Read(ref pointer);
+        }
+
+        public void DeserializeValue(EndianBinaryReader reader)
+        {
+            // Get address to return to after deserializing
+            Pointer currentAddress = reader.GetPositionAsPointer();
+
+            // Jump to address and deserialize values
+            reader.JumpToAddress(pointer);
+            reader.Read(ref value);
+
+            // Return to initial address
+            reader.JumpToAddress(currentAddress);
+        }
+
+        public void Serialize(EndianBinaryWriter writer)
+        {
+            writer.Write(pointer);
+        }
+
+        public void SerializeValue(EndianBinaryWriter writer)
+        {
+            // Ensure correct base address and length is stored
+            pointer.address = writer.GetPositionAsPointer();
+
+            // Write values
+            writer.Write(value);
+        }
+    }
+}
