d3e6e38cadd36d916aa2daf8080fe01a6be5b57c
Raphaël Tétreault <raphael.tetreault@outlook.com>
2024-04-21 14:41:34 -0400

    Implement all casts for Table.GetNext and related functions

--- src/Manifold.Text.Tables/ITableInterface.cs
+++ src/Manifold.Text.Tables/ITableInterface.cs
@@ -4,12 +4,11 @@ namespace Manifold.Text.Tables
 {
     public interface ITableInterface
     {
-        // TODO: write many types no parse passed (ie: just contraints?)
         public string ReadNext();
         public T ReadNext<T>(Func<string, T> parse);
 
-        // No awareness of newline
-        // No awareness of headers?
+        // No awareness of newline 
+        // No awareness of headers? 
         public string WriteNext(string value);
         public string WriteNext<T>(T value);
     }
