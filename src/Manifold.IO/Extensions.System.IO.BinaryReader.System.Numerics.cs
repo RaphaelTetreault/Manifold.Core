091a08fa7302b59b46ae851c189857e401295006
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-08-05 00:46:40 -0400

    Add extension classes for System.Numerics

---
+++ src/Manifold.IO/Extensions.System.IO.BinaryReader.System.Numerics.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+
+namespace Manifold.IO
+{
+
+    public static partial class BinaryWriterExtensions
+    {
+        public static void Write(this EndianBinaryWriter writer, Vector2 value)
+        {
+            writer.Write(value.X);
+            writer.Write(value.Y);
+        }
+
+        public static void Write(this EndianBinaryWriter writer, Vector3 value)
+        {
+            writer.Write(value.X);
+            writer.Write(value.Y);
+            writer.Write(value.Z);
+        }
+
+        public static void Write(this EndianBinaryWriter writer, Vector4 value)
+        {
+            writer.Write(value.X);
+            writer.Write(value.Y);
+            writer.Write(value.Z);
+            writer.Write(value.W);
+        }
+
+        public static void Write(this EndianBinaryWriter writer, Quaternion value)
+        {
+            writer.Write(value.X);
+            writer.Write(value.Y);
+            writer.Write(value.Z);
+            writer.Write(value.X);
+        }
+    }
+}
