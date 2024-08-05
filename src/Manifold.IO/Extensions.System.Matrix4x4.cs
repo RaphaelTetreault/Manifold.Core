57b86f36bdc741bbac6729b5d9665a1e9780238a
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-08-05 00:49:38 -0400

    m: Remove dead code and rename orientation to rotation

--- src/Manifold.IO/Extensions.System.Matrix4x4.cs
+++ src/Manifold.IO/Extensions.System.Matrix4x4.cs
@@ -4,6 +4,12 @@ namespace Manifold.IO
 {
     public static class Matrix4x4Extensions
     {
+        public Matrix4x4 Matrix { get => matrix; set => matrix = value; }
+        public Vector3 Position => matrix.Translation;
+        public Quaternion Rotation => Quaternion.CreateFromRotationMatrix(matrix);
+        public Vector3 RotationEuler => MathFX.ToEulerAngles(Quaternion.CreateFromRotationMatrix(matrix));
+        public Vector3 Scale => matrix;
+
         public static Vector3 Position(this Matrix4x4 matrix)
         {
             //Vector3 position = new Vector3(matrix.M41, matrix.M42, matrix.M43);
@@ -11,17 +17,17 @@ namespace Manifold.IO
             return position;
         }
 
-        public static Quaternion Rotation(this Matrix4x4 matrix)
+        public static Quaternion Orientation(this Matrix4x4 matrix)
         {
             Quaternion orientation = Quaternion.CreateFromRotationMatrix(matrix);
             return orientation;
         }
 
-        public static Vector3 RotationEuler(this Matrix4x4 matrix)
+        public static Vector3 OrientationEuler(this Matrix4x4 matrix)
         {
             Quaternion orientation = Quaternion.CreateFromRotationMatrix(matrix);
             Vector3 euler = MathFX.ToEulerAngles(orientation);
-            return euler;
+            return orientation;
         }
 
         public static Vector3 Scale(this Matrix4x4 matrix)
