4ca9804cee4399c496f2e1a43d7a7b8a8e0057c1
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-12-05 01:21:05 -0500

    Improve Binary File Wrapper (add constructor file read)

--- src/Manifold.IO/BinaryFileWrapper.cs
+++ src/Manifold.IO/BinaryFileWrapper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Manifold.IO
 {
@@ -7,6 +8,31 @@ namespace Manifold.IO
         IBinarySerializable
         where TBinarySerializable : IBinarySerializable, new()
     {
+        // CONSTRUCTORS
+
+        /// <summary>
+        ///     Create a new <see cref="BinaryFileWrapper{TBinarySerializable}"/> with default
+        ///     <see cref="Value"/> and blank <see cref="FileName"/>.
+        /// </summary>
+        public BinaryFileWrapper() { }
+
+        /// <summary>
+        ///     Create a new <see cref="BinaryFileWrapper{TBinarySerializable}"/> where
+        ///     <see cref="Value"/> is loaded from <paramref name="inputPath"/> and
+        ///     <see cref="FileName"/> is set based on the <paramref name="inputPath"/>'s file name.
+        /// </summary>
+        public BinaryFileWrapper(string inputPath)
+        {
+            // Set properties
+            FileName = Path.GetFileNameWithoutExtension(inputPath);
+
+            // Read in new value
+            Value = new TBinarySerializable();
+            using var reader = new EndianBinaryReader(File.OpenRead(inputPath), Endianness);
+            Value.Deserialize(reader);
+        }
+
+
         public abstract Endianness Endianness { get; }
         public abstract string FileExtension { get; }
         public abstract string FileName { get; set; }
