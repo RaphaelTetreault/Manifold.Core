2c63ad70e5c673e1061e5eab80adc2e612859db8
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2025-02-15 20:51:37 -0500

    m: Move analyzer delegate and logfuncfile struct to TableLogger class

---
+++ src/Manifold.IO/TableLogger.cs
@@ -0,0 +1,23 @@
+﻿namespace Manifold.IO;
+
+public static class TableLogger
+{
+    /// <summary>
+    ///     A delegate for analyzing an array of values type <typeparamref name="T"/>,
+    ///     where each value has an associated output file.
+    /// </summary>
+    /// <remarks>
+    ///     Created for use with TableLog types.
+    /// </remarks>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="values"></param>
+    /// <param name="outputFilePath"></param>
+    public delegate void Analyzer<T>(T[] values, string outputFilePath);
+
+    /// <summary>
+    ///     Record of value pair for function which analyzes files with associated file output.
+    /// </summary>
+    /// <param name="AnalysisFunction"></param>
+    /// <param name="FileName"></param>
+    public readonly record struct LogFuncFile<T>(Analyzer<T> AnalysisFunction, string FileName);
+}
