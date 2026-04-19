5eae544de5c2f782e70103aee39c5ce225e00782
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2026-04-19 00:10:14 -0400

    Add Manifold.IO.Windows1252CString

---
+++ src/Manifold.IO/Windows1252CString.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace Manifold.IO;
+
+public class Windows1252CString : CString
+{
+    // CONSTANTS
+    public static readonly Encoding windows1252 = Encoding.GetEncoding(codepage: 1252);
+
+    // PROPERTIES
+    public override Encoding Encoding => windows1252;
+
+    // CONSTRUCTORS
+    public Windows1252CString() : base() { }
+    public Windows1252CString(string value) : base(value) { }
+
+    // OPERATORS
+    public static implicit operator string(Windows1252CString cstr) => cstr.Value;
+    public static implicit operator Windows1252CString(string str) => new(str);
+}
